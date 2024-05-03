67ce84545fb674df8750d0c450c52771f4343183
Ngonidzashe Mangudya <imngonii@gmail.com>
2024-05-03 21:27:06 +0200

    chore: create project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.repopix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:repopix/app/app.dart';
+import 'package:repopix/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
